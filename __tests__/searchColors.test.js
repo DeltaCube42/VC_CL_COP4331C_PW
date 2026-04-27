76d3ff31a44369519312725967646449fbbe3456
DeltaCube42 <jab102003@gmail.com>
2026-04-27 12:09:33 -0400

    A5 Link Changed
    I don't have my droplet and deleted it because I'm not going to pay for having it up for the entire semester when the primary assignment was already graded. I used a friend's droplet link instead, so hopefully this works.

--- __tests__/searchColors.test.js
+++ __tests__/searchColors.test.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 describe("SearchColors API", () => {
   test("returns valid JSON with results array", async () => {
-    const response = await fetch("https://YOUR_DOMAIN_HERE/LAMPAPI/SearchColors.php", {
+    const response = await fetch("https://betinsky-colorslab.com/LAMPAPI/SearchColors.php", { // The link used here is Adam's. My droplet was shut down and deleted awhile back. I'm not remaking all that and paying just for one assignment.
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ search: "red", userId: 1 })
